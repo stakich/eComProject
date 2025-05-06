d3f08506085c2670838d66a7909dfa13d72e768e
stakich <stanislav030507e@gmail.com>
2025-05-06 20:16:30 +0300

    Added media and static files. Also registered model admins

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category, Product
+
+admin.site.register(Category)
+admin.site.register(Product)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
--- store/models.py
+++ store/models.py
@@ -19,5 +19,13 @@ class Product(models.Model):
     brand = models.CharField(max_length=250, default='Unbranded')
     description = models.TextField(blank=True)
     slug = models.SlugField(max_length=250, unique=True)
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=4, decimal_places=2)
+    # image = models.ImageField(upload_to='images/')
+
+    class Meta:
+        verbose_name_plural = 'products'  # plural name for the admin interface
+
+    
+        def __str__(self):
+            return self.title
+    
