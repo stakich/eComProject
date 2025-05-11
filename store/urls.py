daf5a744ec4c0c315c246a07d1ab24257228e10c
stakich <stanislav030507e@gmail.com>
2025-05-11 17:43:37 +0300

    Added template for product info and list category, configured the urls

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name='store'),
+    path('product/<slug:slug>/', views.product_info, name='product_info'),
+    path('search/<slug:category_slug>/', views.list_category, name='list_category'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render # type: ignore
+from django.shortcuts import render, get_object_or_404 # type: ignore
 from .models import Category, Product # type: ignore
 from django.views.generic import ListView, DetailView # type: ignore
 from django.http import HttpResponse # type: ignore
 
+
 def store(request):
-    return render(request, 'store/store.html')
+
+    all_products = Product.objects.all()
+
+    
+    return render(request, 'store/store.html', context={'all_products': all_products})
 
 def category_list(request):
     all_categories = Category.objects.all()
     return {'all_categories': all_categories}
+
+def product_info(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'store/product_info.html', context={'product': product}) 
+
+def list_category(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    return render(request, 'store/list-category.html', context={'category': category, 'products': products})
